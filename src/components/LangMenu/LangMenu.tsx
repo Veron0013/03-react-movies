6e22ba69742907673c35a579bdbd45d7b42770b2
Mr.Igor <iv_mirsoft@ukr.net>
2025-08-03 09:49:33 +0300

    lang settings fix

--- src/components/LangMenu/LangMenu.tsx
+++ src/components/LangMenu/LangMenu.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react"
+//import { useState, useRef, useEffect } from "react"
 import css from "./LangMenu.module.css"
 
 interface LanguageProps {
@@ -6,22 +6,22 @@ interface LanguageProps {
 }
 
 export default function DropdownMenu({ onSelect }: LanguageProps) {
-	const [isOpen, setIsOpen] = useState(false)
-	const menuRef = useRef<HTMLDivElement>(null)
+	//const [isOpen, setIsOpen] = useState(false)
+	//const menuRef = useRef<HTMLDivElement>(null)
 
-	const toggleMenu = () => setIsOpen((prev) => !prev)
+	//const toggleMenu = () => setIsOpen((prev) => !prev)
 
-	// Закриття по кліку поза меню
-	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-				setIsOpen(false)
-			}
-		}
+	//// Закриття по кліку поза меню
+	//useEffect(() => {
+	//	const handleClickOutside = (event: MouseEvent) => {
+	//		if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+	//			setIsOpen(false)
+	//		}
+	//	}
 
-		document.addEventListener("mousedown", handleClickOutside)
-		return () => document.removeEventListener("mousedown", handleClickOutside)
-	}, [onSelect])
+	//	document.addEventListener("mousedown", handleClickOutside)
+	//	return () => document.removeEventListener("mousedown", handleClickOutside)
+	//}, [onSelect])
 
 	function handleClick(langValue: string): void {
 		onSelect(langValue)
@@ -33,7 +33,7 @@ export default function DropdownMenu({ onSelect }: LanguageProps) {
 				<li className={css.menu__item} onClick={() => handleClick("uk-UA")}>
 					UA
 				</li>
-				<li className={css.menu__item} onClick={() => handleClick("en-US")}>
+				<li className={css.menu__item} onClick={() => handleClick("uk-UA")}>
 					EN
 				</li>
 			</ul>
